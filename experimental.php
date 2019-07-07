bfdc95e495416f0a00800cde0c3b1ffe5c101406
Arthur Melkonyan <45238571+artmelkon@users.noreply.github.com>
2019-07-07 00:08:44 -0700

    initial upload

---
+++ experimental.php
@@ -0,0 +1,38 @@
+<?php
+/* 
+* Template Name: Epxerimental Page
+*/
+
+
+get_header();
+
+?>
+<!-- Page Title
+============================================= -->
+<section id="page-title">
+  <div class="container clearfix">
+    <h1>Eperimental Code</h1>
+  </div>
+</section><!-- #page-title end -->
+
+<!-- Content
+============================================= -->
+<section id="content">
+
+  <div class="content-wrap">
+
+    <div class="container clearfix">
+
+      <?php
+      
+
+
+      ?>
+
+    </div><!-- .postcontent end -->
+
+  </div>
+
+</section><!-- #content end -->
+
+<?php get_footer(); ?>
